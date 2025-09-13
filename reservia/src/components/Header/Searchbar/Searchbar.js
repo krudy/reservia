65a862084aef41aef9dc684dd7d1ff0f5fc72e23
krudy <krzysztof.rudnicki678@gmail.com>
2025-09-13 13:46:53 +0200

    feat:enhance header with background image and add styled hotel card

--- reservia/src/components/Header/Searchbar/Searchbar.js
+++ reservia/src/components/Header/Searchbar/Searchbar.js
@@ -6,8 +6,8 @@ function Searchbar() {
 
         <div className='d-flex'>
 
-            <input className={`form-control`} text='text' placeholder='Search...'></input>
-            <button className='btn btn-primary'>Search</button>
+            <input className="form-control" text='text' placeholder='Search...'></input>
+            <button className='ms-1 btn btn-secondary'>Search</button>
 
         </div>
     )
--- reservia/src/components/Hotels/Hotel/Hotel.js
+++ reservia/src/components/Hotels/Hotel/Hotel.js
@@ -1,10 +1,36 @@
-import React  from "react";
+import React from "react";
 import styles from './Hotel.module.css'
+import hotelImg from '../../../assets/images/hotel.jpg'
 
 function Hotel() {
     return (
-        <div className={styles.hotel}>
-            hotel
+        <div className={`${styles.hotel} card`}>
+            <div className="card-body">
+                <div className="row">
+                    <div className="col-4">
+                        <img src={hotelImg} alt="" className="img-fluid img-thumbnail" />
+
+                    </div>
+                    <div className="col-8">
+                        <div className="row">
+                            <div className="col">
+                                <p className={`${styles.title}`}>Pensjonat</p>
+                                <span class="badge bg-light text-dark">Warszawa</span>
+                            </div>
+                            <div className="col text-end">
+                                <h5>Ocena: 8.3</h5>
+                                <a href="#!" className="btn btn-primary mt-2 px-5 ">show</a>
+
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-12">
+                        <p className={`${styles.description}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae architecto pariatur accusamus non nisi fugit qui neque, voluptate omnis consectetur laboriosam eos cumque deserunt natus reiciendis totam. Esse, error a?</p>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
